5f13fde11df1a5a018fac552237547e320910207
Masa <masa.beydoun@hotmail.com>
2025-01-14 13:08:48 +0300

    returns

--- gen/LispLexer.java
+++ gen/LispLexer.java
@@ -219,6 +219,7 @@ public class LispLexer extends Lexer {
 
 			        throw new RuntimeException("Unclosed string literal at line " + getLine() + ", column " + getCharPositionInLine());
 			    
+			break;
 		}
 	}
 	private void FORMAT_STRING_action(RuleContext _localctx, int actionIndex) {
@@ -240,6 +241,7 @@ public class LispLexer extends Lexer {
 
 			        throw new RuntimeException("Unclosed format string literal at line " + getLine() + ", column " + getCharPositionInLine());
 			    
+			break;
 		}
 	}
 
